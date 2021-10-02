4c9164583193b3291ee8d5c21d324bf2dacf8849
buddhiamigo <buddhis@99x.lk>
2021-10-02 23:40:15 +0530

    init cdk boilerplate and jenkins file

---
+++ cdk-boilerplate/src/CdkBoilerplate/CdkBoilerplateStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace CdkBoilerplate
+{
+    public class CdkBoilerplateStack : Stack
+    {
+        internal CdkBoilerplateStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
---
+++ cdk-boilerplate/src/CdkBoilerplate/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CdkBoilerplate
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new CdkBoilerplateStack(app, "CdkBoilerplateStack", new StackProps());
+            app.Synth();
+        }
+    }
+}
